fc13710621e823876db8275a2f2131165e674d76
Salakar <mike.diarmid@gmail.com>
2018-02-26 11:30:24 +0000

    routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 /* eslint-disable global-require */
 
-// TODO handle from node env?
-const BRAND = 'cm';
+const types = ['post', 'page', 'category'];
+
+const filter = {
+  type: types,
+  // status: 'publish', // TODO add back in when pages published.
+};
 
 module.exports = {
   /** ------------
@@ -10,19 +14,41 @@ module.exports = {
 
   'get *': async (req, res) => {
     const url = req.params[0];
+    const [, slug, childSlug, babySlug] = url.split('/');
 
-    if (url.includes('_next')) {
+    // allow next to handle it's own routes and assets
+    if (slug === '_next' || slug.includes('.')) {
       return next.getRequestHandler()(req, res);
     }
 
-    // TODO URL mapping to content slug
-    if (url === '/') {
-      return next.render(req, res, `/${BRAND}/templates/homepage`, {
-        "slug": "homepage",
-        "title": "Winner winner chicken dinner",
-        "meta": JSON.parse("{\"template\":\"homepage\",\"block_hero\":{\"headline\":\"Homepage\",\"secondary\":\"Welcome to checkd media\"},\"block_about\":{\"cta\":\"Read More\",\"description\":\"established communities await your brand.\"},\"has_sub_pages\":false}"),
-        "parent": 0
-      });
+    const promises = [];
+
+    promises.push(Content.findOne({ slug: slug || 'homepage', ...filter }));
+    if (childSlug)
+      promises.push(Content.findOne({ slug: childSlug, ...filter }));
+    if (babySlug) promises.push(Content.findOne({ slug: babySlug, ...filter }));
+
+    const [error, contents] = await A2A(promises);
+    if (error) {
+      return next.render(req, res, `/${BRAND}/http/error`, error);
+    }
+
+    if (contents && contents[0]) {
+      const { id, slug: contentSlug, meta } = contents[0];
+      if (meta.template) {
+        return next.render(
+          req,
+          res,
+          `/${BRAND}/templates/${meta.template}`,
+          contents[2] || contents[1] || contents[0]
+        );
+      }
+
+      console.warn('-'.repeat(80));
+      console.warn(
+        ` -> FIXME: Template meta field missing for content '${id}' with slug '${contentSlug}'`
+      );
+      console.warn('-'.repeat(80));
     }
 
     // 404 page

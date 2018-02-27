798580557aa8be1a9e88fb8bb06ad774e464fd13
Rusticman <russellwheatley85@gmail.com>
2018-02-27 11:31:06 +0000

    posts now rendering

--- src/pages/cm/templates/news/index.js
+++ src/pages/cm/templates/news/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Page from '../../layout/Page';
 import Header from '../../layout/Header';
 import Hero from '../../layout/Hero';
 import Posts from './Posts';
@@ -20,7 +21,10 @@ class News extends Component {
   }
 
   render() {
-    const { meta: { block_hero: { headline, secondary } } } = this.props.news;
+    const { posts, news } = this.props;
+
+    const headline = idx(this.props.news.meta.block_hero.headline, headline => headline);
+    const secondary = idx(this.props.news.meta.block_hero.secondary, secondary => secondary);
     return (
       <React.Fragment>
         <Header
@@ -29,8 +33,8 @@ class News extends Component {
         />
         <Hero
           image="https://images.unsplash.com/photo-1448932223592-d1fc686e76ea?auto=format&fit=crop&w=1350&q=80"
-          headline={headline}
-          description={secondary}
+          headline={headline || 'no headline'}
+          description={secondary || 'no description'}
         />
         <Posts posts={this.props.posts} />
       </React.Fragment>
@@ -38,4 +42,4 @@ class News extends Component {
   }
 }
 
-export default News;
+export default Page(News);
---
+++ src/pages/cm/templates/post/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Page from '../../layout/Page';
+import idx from 'idx';
+import Header from '../../layout/Header';
+import Hero from '../../layout/Hero';
+import DraftRender from '../../../../components/DraftRender';
+import { mediaUrl } from '../../../../helpers';
+import styles from './styles.less';
+
+class Post extends React.Component {
+  render() {
+    const post = this.props;
+    const featureImg = idx(post, p => p.meta.featured_image);
+    return (
+      <React.Fragment>
+        <Header />
+        <div className={styles.container}>
+          <Hero
+            headline={post && post.title || ''}
+            image={featureImg ? mediaUrl(featureImg, 'xl') : ''}
+          />
+          <div className={styles.post}>
+            {idx(post, p => p.meta.editor_content_state) ?
+              <DraftRender draftContext={post.meta.editor_content_state} />:
+              <div>Currently, there is no content to render for this post.</div>
+            }
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Page(Post);

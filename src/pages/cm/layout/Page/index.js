c7f7b02cf9065bc72cd6d93c347afdb79b51183b
Rusticman <russellwheatley85@gmail.com>
2018-02-27 13:06:50 +0000

    page component update

--- src/pages/cm/layout/Page/index.js
+++ src/pages/cm/layout/Page/index.js
@@ -9,22 +9,29 @@ import './styles.less';
 const FAV_ICON = 'https://cdn.checkd.media/favicons/cm.png';
 
 const Page = ComposedComponent => {
-  function Decorator({ url }) {
+  function Decorator(props) {
+    const extendedProps = {
+      ...props,
+    };
+    const { url: { query } } = props;
+    extendedProps[query.meta.template] = query;
     return (
       <Document>
         <Head>
-          <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:300,400"
+            rel="stylesheet"
+          />
           <link rel="shortcut icon" type="image/png" href={FAV_ICON} />
           <link rel="shortcut icon" href={FAV_ICON} />
           <link rel="apple-touch-icon" href={FAV_ICON} />
         </Head>
-        <ComposedComponent {...url.query} />
+        <ComposedComponent {...extendedProps} />
       </Document>
     );
   }
 
   hoistNonReactStatics(Decorator, ComposedComponent);
-
   return Decorator;
 };
 

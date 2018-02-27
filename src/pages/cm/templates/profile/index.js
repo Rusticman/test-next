90313872ed0878dc7df1e6de6dc4cb3786511c09
Rusticman <russellwheatley85@gmail.com>
2018-02-27 16:16:08 +0000

    profile functional component

--- src/pages/cm/templates/profile/index.js
+++ src/pages/cm/templates/profile/index.js
@@ -6,38 +6,26 @@ import Page from '../../layout/Page';
 import Who from './Who';
 import Questions from './Questions';
 import styles from './styles.less';
-import AboutMe from "./AboutMe";
+import AboutMe from './AboutMe';
 
-class Profile extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      push: 0,
-      pull: 0,
-    };
-  }
-
-  render() {
-    console.log('PROFILE', this.props);
-    const { profile } = this.props;
-    return (
-      <React.Fragment>
-        <Header
-          transparent
-          offsetTop={10}
-        />
-        <Hero
-          headline={'Profile'}
-        />
-        <div className={styles.container}>
-          <Who profile={profile} />
-          <AboutMe profile={profile} />
-          <Questions profile={profile} />
-        </div>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+function Profile({ profile }) {
+  return (
+    <React.Fragment>
+      <Header
+        transparent
+        offsetTop={10}
+      />
+      <Hero
+        headline={'Profile'}
+      />
+      <div className={styles.container}>
+        <Who profile={profile} />
+        <AboutMe profile={profile} />
+        <Questions profile={profile} />
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default Page(Profile);
